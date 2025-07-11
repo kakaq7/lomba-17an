870a3ad410bd5eb3abaad2b592347a91713442b5
kakaq7 <93967645+kakaq7@users.noreply.github.com>
2025-07-12 02:57:14 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+# Aplikasi Manajemen Lomba & Absensi Karang Taruna
+# (versi final lengkap akan disisipkan di sini pada update lanjutan)
+import streamlit as st
+
+st.title("Karang Taruna Bina Bhakti")
+st.write("Aplikasi siap dikembangkan lebih lanjut.")
