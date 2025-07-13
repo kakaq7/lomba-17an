c150cdbf9b3fd3c296431831aa6c12c00e1f669b
kakaq7 <93967645+kakaq7@users.noreply.github.com>
2025-07-14 01:25:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,38 +5,6 @@ from datetime import datetime
 from pytz import timezone
 from fpdf import FPDF
 
-st.markdown("""
-    <style>
-    html, body, [class*="css"]  {
-        font-size: 16px;
-    }
-
-    h1, h2, h3, h4 {
-        font-size: 28px !important;
-        font-weight: bold;
-    }
-
-    .title {
-        font-size: 100px !important;
-        font-weight: bold;
-        color: blue;
-    }
-
-    .stTextInput > div > input {
-        font-size: 16px;
-    }
-
-    .stSelectbox > div {
-        font-size: 16px;
-    }
-
-    .stButton > button {
-        font-size: 16px;
-    }
-    </style>
-""", unsafe_allow_html=True)
-
-
 # Zona Waktu Indonesia
 wib = timezone("Asia/Jakarta")
 
