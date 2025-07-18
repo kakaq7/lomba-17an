a5f78a1fd063529af4c8b29729e785dba1102676
kakaq7 <93967645+kakaq7@users.noreply.github.com>
2025-07-19 00:36:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def proses_login():
         st.session_state.login_error = "Username atau password salah."
 
 # Login/Register
-st.title("🇮🇩Aplikasi Karang bbTaruna Bina Bhakti")
+st.title("🇮🇩Aplikasi Karang nnTaruna Bina Bhakti")
 if not st.session_state.login:
     mode = st.selectbox("Pilih", ["Login", "Daftar Akun"])
     if mode == "Login":
