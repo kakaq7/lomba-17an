c70c0713ec9071de81528d28f3650ac8340a88dd
kakaq7 <93967645+kakaq7@users.noreply.github.com>
2025-07-19 22:15:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -272,7 +272,7 @@ if not st.session_state.get("login"):
                     # Reset session
                     st.session_state.otp_sent_daftar = False
                     st.session_state.otp_code_daftar = ""
-                    st.session_state.update({"Pilih": "Login", "mode": "Login"})
+                    st.session_state.update({"Pilih": "Login"})
                     time.sleep(2)
                     st.rerun()
 
